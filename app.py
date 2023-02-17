c02fd0f9ed64425da4f0b3ba6942a1ca87d57e3e
tfujikawa@japan <58358482+2011tfujikawa@users.noreply.github.com>
2023-02-17 17:11:53 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+import yfinance as yf
+
+from dateutil.relativedelta import relativedelta
+
+st.title("5 years Monthly anomalies")
+codelist = st.multiselect(
+    'Symbols@Yahoo Finance! US',
+    ['2501.T', '2502.T', '2503.T'],
+    ['2501.T', '2502.T', '2503.T'])
+
+end_D = datetime.date.today()
+start_D = datetime.date.today() - relativedelta(years=5)
+
+df = yf.download(codelist, start=start_D, end=end_D, interval='1mo')["Adj Close"]
+print(df)
+
+df_analyse=df.pct_change().dropna().groupby([lambda x: x.month]).sum()
+print(df_analyse)
+
+st.bar_chart(df_analyse)
+st.dataframe(df_analyse.T)
